50450479573f34fc58e003439f680e50f91cccb8
Theodore Riera <warsang@amazon.com>
2021-02-11 17:10:38 +0000

    Changed variable name for container to current_container (as that's what I'd been using)

--- custom_functions/drafts/task4customFunc.py
+++ custom_functions/drafts/task4customFunc.py
@@ -24,10 +24,10 @@ def task4customFunc(current_container=None, peer=None, priority=None, count=None
     list_name = "temp_peer_list_%s" % current_container
     
     # You need the container object in order to update it.
-    update_container = phantom.get_container(container)
+    update_container = phantom.get_container(current_container)
     
     # Get the data node of the container
-    data = phantom.get_container(container)['data']
+    data = phantom.get_container(current_container)['data']
     data.update({"peer_list":list_name})
     phantom.update(update_container, {'data':data} )
     phantom.remove_list(list_name)
